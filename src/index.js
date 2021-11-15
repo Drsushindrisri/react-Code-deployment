409869c172f8806ef9e9920ac823ada5e3efb989
Bavithra <bavithra.r@s10safecare.com>
2021-11-15 21:51:58 +0530

    react web page

--- src/index.js
+++ src/index.js
@@ -1,42 +1,19 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { App } from "./AppNew";
-import DoctorsList from "./DoctorsList";
-import Header from "./header";
-import SlotBookAppointment from "./BookAppointment";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import PaymentConsultation from "./Consultationpayment";
-import "./index.css";
 
-function RootApp() {
-  useEffect(() => {
-    if (!sessionStorage.getItem("userId")) {
-      sessionStorage.setItem("userId", window.location.pathname);
-    }
-  }, []);
-
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route exact path="/doctors-list" component={DoctorsList} />
-          <Route
-            exact
-            path="/Slot-BookAppointment"
-            component={SlotBookAppointment}
-          />
-          <Route exact path="/OrgDoctorFees" component={PaymentConsultation} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-ReactDOM.render(<RootApp />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
