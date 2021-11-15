564ae5d40ae4ce9b1fd64efce3996b5d688514d9
Bavithra <bavithra.r@s10safecare.com>
2021-11-15 22:11:45 +0530

    react web page-new

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AppBar from './appbar';
+import Facebook from './facebook';
+
+function App() {
+  return (
+    <div className="App">
+     
+      
+      <AppBar />
+      <Facebook />
+    </div>
+  );
+}
+
+export default App;
