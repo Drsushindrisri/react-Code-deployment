b8f842623fb4cc6e4b31981a087b9803ac06a5d5
Aditya V <aditya.v@s10health.com>
2021-11-22 18:00:09 +0530

    added blogs

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { uid } from "react-uid";
 import { fetchData } from "./Api/Apis";
+import Carousel from "react-elastic-carousel";
+import styles from "./Blogs.module.scss";
 
-const Blogs = () => {
+const Blogs = ({ history }) => {
   const [blogsList, setBlogsList] = useState([]);
 
   useEffect(() => {
@@ -17,10 +19,34 @@ const Blogs = () => {
   };
 
   return (
-    <div>
-      {blogsList.map((item, ind) => (
-        <div key={uid(ind)}></div>
-      ))}
+    <div className={styles.blogs__main}>
+      <Carousel
+        itemsToScroll={1}
+        itemsToShow={1}
+        showArrows={false}
+        enableAutoPlay
+        itemPadding={[0, 10, 0, 10]}
+      >
+        {blogsList.map((item, ind) => (
+          <div
+            key={uid(ind)}
+            onClick={() =>
+              history.push({
+                pathname: "/blog",
+                state: { blogId: item.blogId },
+              })
+            }
+            className={styles.blogs__blogItem}
+          >
+            <img
+              src="https://staging.s10health.in/contentImg/960x540/16355732232330.jpg"
+              alt=""
+              height={150}
+            />
+            {item.blog_title}
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
