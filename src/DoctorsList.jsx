d433c744cd2df2e3b1126ed9563a982cd9dfc3f3
Aditya V <aditya.v@s10health.com>
2021-11-26 17:48:37 +0530

    new doctorsList

--- src/DoctorsList.jsx
+++ src/DoctorsList.jsx
@@ -1,16 +1,33 @@
 import { useEffect, useState } from "react";
-import styles from "./sass/AppNew.module.scss";
+import styles from "./sass/DoctorsList.module.scss";
+import { RiSearchLine } from "react-icons/ri";
 import { uid } from "react-uid";
 import { fetchData } from "./Api/Apis";
-// import Footer from "./footer";
-// import SlotBookAppointment from "./BookAppointment";
+
+const filterItems = [
+  "All",
+  "Cardiology",
+  "Neurology",
+  "Dermatology",
+  "Pediatric",
+];
 
 export const DoctorsList = (props) => {
   const [doctors, setDoctors] = useState({ real: [], modify: [] });
+  const [selectedFilter, setSelectedFilter] = useState("");
 
   useEffect(() => {
     getDoctorsList();
   }, []);
+
+  useEffect(() => {
+    if (selectedFilter === "All") {
+      setDoctors({ ...doctors, modify: doctors.real });
+    } else {
+      handleFilter();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedFilter]);
 
   async function getDoctorsList() {
     try {
@@ -19,56 +36,94 @@ export const DoctorsList = (props) => {
     } catch (error) {}
   }
 
+  const handleFilter = () => {
+    const temp = doctors.real.reduce((acc, item) => {
+      if (item.speciality_name === selectedFilter) {
+        acc.push(item);
+      }
+      return acc;
+    }, []);
+    setDoctors({ ...doctors, modify: temp });
+  };
+
+  const handleSearch = (searchText) => {
+    if (searchText) {
+      const temp = doctors.real.reduce((acc, item) => {
+        for (const key in item) {
+          if (item[key].toLowerCase().includes(searchText.toLowerCase())) {
+            acc.push(item);
+          }
+        }
+
+        return acc;
+      }, []);
+      setDoctors({ ...doctors, modify: temp });
+    } else {
+      setDoctors({ ...doctors, modify: doctors.real });
+    }
+  };
+
   return (
-    <div className={styles.Doctors_Container}>
-      {(Array.isArray(doctors.modify) ? doctors.modify : []).map(
-        (item, ind) => (
-          <div className={styles.DoctorsList_Items} key={uid(ind)}>
-            <img className={styles.Doctors_Image} src={item.image} alt="" />
-            <span style={{ width: "10px" }}></span>
+    <div className={`page-content ${styles.doctorsList__main}`}>
+      <h3>Doctors</h3>
+      <div className={styles.doctorsList__searchBar}>
+        <RiSearchLine />
+        <input
+          placeholder="Search"
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
+      <div className={styles.doctorsList__specalities}>
+        {filterItems.map((item, ind) => (
+          <div
+            className={`${styles.doctorsList__specalityItem} ${
+              selectedFilter === item &&
+              styles.doctorsList__specalityItemSelected
+            }`}
+            key={uid(ind)}
+            onClick={() => {
+              if (item !== selectedFilter) {
+                setSelectedFilter(item);
+              }
+            }}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className={styles.doctorsList__container}>
+        {doctors.modify.map((item, ind) => (
+          <div
+            className={styles.doctorsList__item}
+            key={uid(ind)}
+            onClick={() => {
+              props.history.push({
+                pathname: "/Slot-BookAppointment",
+                state: {
+                  name: item.docName,
+                  image: item.docProfileImg,
+                  speciality: item.speciality_name,
+                },
+              });
+            }}
+          >
+            <img
+              src="https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              alt={item.docName}
+            />
             <div>
-              <h4 className={styles.Doctors_Names}> {item.docName}</h4>
-              <p>
-                <span className={styles.doctors_Pgblock}>
-                  PG in Nutrition & Dietetics
-                </span>
-                <p className={styles.doctors_Specialitynames}>
-                  {item.speciality_name}
-                </p>
+              <p>{item.docName}</p>
+              <p className={styles.doctorsList__qualification}>
+                {item.docEduQualification.slice(0, 20)}
+                {item.docEduQualification.length > 20 && "..."}
               </p>
-              <div className="flex-container">
-                <button
-                  onClick={() => {
-                    props.history.push({
-                      pathname: "/Slot-BookAppointment",
-                      state: {
-                        name: item.docName,
-                        image: item.docProfileImg,
-                        speciality: item.speciality_name,
-                      },
-                    });
-                  }}
-                  className={styles.DoctorsAppointment_Booknow}
-                >
-                  {" "}
-                  Book Now
-                </button>
-                {""}
-                {""}
-                <button
-                  onClick={() => {
-                    props.history.push("/OrgDoctorFees");
-                  }}
-                  className={styles.Doctors_Consultant}
-                >
-                  Consultant Online
-                </button>
-              </div>{" "}
+              <p>{item.docWorkLocHosName}</p>
             </div>
           </div>
-        )
-      )}
+        ))}
+      </div>
     </div>
   );
 };
+
 export default DoctorsList;
