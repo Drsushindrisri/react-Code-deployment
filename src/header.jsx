acbab428f58cf101896e5ec84748ab644cdfea58
Aditya V <aditya.v@s10health.com>
2021-12-18 11:15:28 +0530

    header gradient

--- src/header.jsx
+++ src/header.jsx
@@ -53,34 +53,14 @@ export default Header;
 const WaveSvg = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 80 1125 245">
     <defs>
-      <linearGradient
-        id="a"
-        x2="1124.577"
-        y1="131.552"
-        y2="131.552"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#00b2b8" />
-        <stop offset=".817" stopColor="#56ced4" />
-      </linearGradient>
-      <linearGradient
-        id="b"
-        x1="562.5"
-        x2="562.5"
-        y1="235.37"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#ffc559" />
-        <stop offset="1" stopColor="#ff8717" />
+      <linearGradient id="MyGradient" x1="0" x2="0" y1="0" y2="1">
+        <stop offset="30%" stop-color="hsla(0, 0%, 100%, 1)" />
+        <stop offset="55%" stop-color="hsla(32, 100%, 50%, 1)" />
       </linearGradient>
     </defs>
     <path
-      fill="url(#a)"
+      fill="url(#MyGradient)"
       d="M0 18.867v180.176s280.024 101.685 555.478 0c248.194-104.165 569.1 19.01 569.1 19.01V18.867Z"
-    />
-    <path
-      fill="url(#b)"
-      d="M0 0v190.176s280.024 101.686 555.478 0c248.194-104.165 569.1 19.01 569.1 19.01L1125 0Z"
     />
   </svg>
 );
