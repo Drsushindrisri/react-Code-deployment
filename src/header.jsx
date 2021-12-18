1584d49fca9393b96d9696a647ce72d96fc9bf76
Aditya V <aditya.v@s10health.com>
2021-12-18 11:20:13 +0530

    header white

--- src/header.jsx
+++ src/header.jsx
@@ -52,15 +52,6 @@ export default Header;
 
 const WaveSvg = () => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 80 1125 245">
-    <defs>
-      <linearGradient id="MyGradient" x1="0" x2="0" y1="0" y2="1">
-        <stop offset="30%" stop-color="hsla(0, 0%, 100%, 1)" />
-        <stop offset="55%" stop-color="hsla(32, 100%, 50%, 1)" />
-      </linearGradient>
-    </defs>
-    <path
-      fill="url(#MyGradient)"
-      d="M0 18.867v180.176s280.024 101.685 555.478 0c248.194-104.165 569.1 19.01 569.1 19.01V18.867Z"
-    />
+    <path d="M0 18.867v180.176s280.024 101.685 555.478 0c248.194-104.165 569.1 19.01 569.1 19.01V18.867Z" />
   </svg>
 );
